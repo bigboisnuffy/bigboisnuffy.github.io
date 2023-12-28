1a9e63b2157a35c8d2b7952e105178c46678660e
bigboisnuffy <snuffy09765@gmail.com>
2023-12-28 07:49:01 +0000

    imdsm

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const BLOB = document.getElementById("BLOB");
-
-document.body.onpointermove = event => {
-  const { clientX, clientY } = event;
-  BLOB.animate({
-    left: `${clientX}px`,
-    top : `${clientY}px`
-  },{duration: 3000, fill:"forwards"});
-
-};
-
-
 
 document.querySelector("h1").onmouseover = event => {
     let iterations = 0;
