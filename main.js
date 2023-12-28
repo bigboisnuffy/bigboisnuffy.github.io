174669d7d30d678240df1e227b9e637aa20389b1
bigboisnuffy <snuffy09765@gmail.com>
2023-12-28 07:20:36 +0000

    fdicxxxx

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ document.body.onpointermove = event => {
   },{duration: 3000, fill:"forwards"});
 
 };
+
+
+
+document.querySelector("h1").onmouseover = event => {
+    let iterations = 0;
+    const interval = setInterval(()=>{
+       event.target.innerText = event.target.innerText.split("").map((letter, index) => {
+         if(index < iterations){
+           return event.target.dataset.value[index];
+         }
+         return letters[Math.floor(Math.random() * 26)];
+               
+       }).join("");
+      if(iterations >= event.target.dataset.value.length){
+        clearInterval(interval);
+          }
+   iterations += 1 / 3;
+    }, 30)
+   
+  }
